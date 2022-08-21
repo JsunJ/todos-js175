91b2a19b27c092e376baa70843452022b14468fd
Jason Jones <JsunJ.Jones@gmail.com>
2022-08-21 03:16:52 -0400

    Add todo list display on homepage plus case-insensitive sorting

--- todos.js
+++ todos.js
@@ -5,13 +5,42 @@ const app = express();
 const host = "localhost";
 const port = 3000;
 
+// Static data for initial testing
+let todoLists = require("./lib/seed-data");
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
+app.use(express.static("public"));
+
+const compareByTitle = (todoListA, todoListB) => {
+  let titleA = todoListA.title.toLowerCase();
+  let titleB = todoListB.title.toLowerCase();
+
+  if (titleA < titleB) {
+    return -1;
+  } else if (titleA > titleB) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+const sortTodoLists = lists => {
+  let undoneLists = lists.filter(list => !list.isDone());
+  let doneLists = lists.filter(list => list.isDone());
+
+  undoneLists.sort(compareByTitle);
+  doneLists.sort(compareByTitle);
+
+  return [...undoneLists, ...doneLists];
+};
 
 app.get("/", (req, res) => {
-  res.render("lists");
+  res.render("lists", {
+    todoLists: sortTodoLists(todoLists),
+  });
 });
 
 app.listen(port, host, () => {
